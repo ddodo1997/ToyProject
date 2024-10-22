d364699cefe2b9471839b62ad39f4633e71ac4f1
김승욱 <ddodo1997@naver.com>
2024-10-22 16:23:24 +0900

    프레임워크 제외.

--- SFML Template/main.cpp
+++ SFML Template/main.cpp
@@ -1,9 +1,48 @@
-﻿#include "stdafx.h"
-
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
 int main()
 {
-	FRAMEWORK.Init(1920,1080,"SFML Framework!!!");
-	FRAMEWORK.Do();
-	FRAMEWORK.Release();
+	sf::VideoMode vm(1920, 1080);
+	sf::RenderWindow window(vm, "Flappy bird");
+
+
+	while (window.isOpen())
+	{
+		//매 프레임 초기화
+
+		//이벤트
+		sf::Event ev;
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+
+			case sf::Event::KeyPressed:
+				switch (ev.key.code)
+				{
+				case sf::Keyboard::Escape:
+				window.close();
+				break;
+				default:
+					break;
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+
+
+		//업데이트
+
+		window.clear();
+		//드로우
+		window.display();
+	}
+
 	return 0;
 }
